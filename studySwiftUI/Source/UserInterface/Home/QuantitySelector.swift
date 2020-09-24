fbf6259cfac004c40c7b552bb9ecf2ce9fac8f8a
p41155a <50395024+p41155a@users.noreply.github.com>
2020-09-24 11:49:31 +0900

    [판매 상세] stepper에 햅틱 피드백 적용

--- studySwiftUI/Source/UserInterface/Home/QuantitySelector.swift
+++ studySwiftUI/Source/UserInterface/Home/QuantitySelector.swift
@@ -11,6 +11,9 @@ struct QuantitySelector: View {
   @Binding var quantity: Int
   var range: ClosedRange<Int> = 1...20 // 수량 선택 가능 범위
   
+  private let softFeedback = UIImpactFeedbackGenerator(style: .soft) // 부드러운 느낌의 진동
+  private let rigidFeedback = UIImpactFeedbackGenerator(style: .rigid) // 딱딱한 느낌의 진동
+  
   // MARK: Body
   
   var body: some View {
@@ -40,6 +43,11 @@ struct QuantitySelector: View {
   private func changeQuantity(_ num: Int) {
     if range ~= quantity + num {
       quantity += num
+      softFeedback.prepare() // 진동 지연 시간을 줄일 수 있도록 미리 준비 시키는 메소드
+      softFeedback.impactOccurred(intensity: 0.8) // 진동 세기
+    } else {
+      rigidFeedback.prepare()
+      rigidFeedback.impactOccurred()
     }
   }
 }
